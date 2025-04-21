d3576c66e6937314e80dc592fb8578994e8a113c
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-04-21 15:19:47 -0700

    done with GET business by ID

--- main.py
+++ main.py
@@ -26,5 +26,15 @@ def get_businesses():
         business['id'] = business.key.id
     return jsonify(businesses), 200
 
+@app.route('/businesses/<int:business_id>', methods=['GET'])
+def get_business(business_id):
+    business_key = datastore_client.key('Business', business_id)
+    business = datastore_client.get(business_key)
+
+    if not business:
+        return jsonify({'Error': 'No business with this business_id exists'}), 404
+    
+    return jsonify(business), 200
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
