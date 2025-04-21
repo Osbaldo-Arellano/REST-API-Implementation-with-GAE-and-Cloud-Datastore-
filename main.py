3470aaf2dcff8cfb1f7a1756715fb405bce4803d
Osbaldo-Arellano <osbaldoarellano1996@gmail.com>
2025-04-21 16:34:13 -0700

    GET for business based on owner ID

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def get_business_by_id(business_id):
 @app.route('/businesses/<int:business_id>', methods=['PUT'])
 def edit_business(business_id):
     data = request.get_json()
-    required_attrs = ['owner_id', 'name', 'street_address', 'city', 'state', 'zip_code']
-    if not all(attr in data for attr in required_attrs):
+    required_fields = ['owner_id', 'name', 'street_address', 'city', 'state', 'zip_code']
+    if not all(field in data for field in required_fields):
         return jsonify({'Error': 'The request body is missing at least one of the required attributes'}), 400
     
     # Get entry from Datastore
@@ -72,7 +72,7 @@ def delete_business(business_id):
     
     # Get and delete all of the business reviews 
     query = datastore_client.query(kind='Review')
-    query.add_filter('business_id', '=', business_id)
+    query.add_filter(filter=('business_id', '=', business_id))
     reviews = query.fetch()
     for review in reviews:
         datastore_client.delete(review.key)
@@ -81,6 +81,18 @@ def delete_business(business_id):
 
     # just return the status code 204 (No Content [Success])
     return '', 204
+
+@app.route('/owners/<int:owner_id>/businesses', methods=['GET'])
+def list_owner_businesses(owner_id):
+    query = datastore_client.query(kind='Business')
+    query.add_filter(filter=('owner_id', '=', owner_id))
+    businesses = list(query.fetch())
+
+    # Add ID to the response
+    for buiness in businesses:
+        buiness['id'] = buiness.key.id
+
+    return jsonify(businesses), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
